ce8eac3cc3d1b119874ccc27799b123e1d0f9bef
Laís Cristine Bordallo Pinheiro <laiscbordallo@gmail.com>
2024-08-13 11:52:17 -0300

    finalização do about e modificações carrossel

--- src/app/content/carrossel/carrossel.component.ts
+++ src/app/content/carrossel/carrossel.component.ts
@@ -1,13 +1,12 @@
-import { Component, AfterViewInit  } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+
 
 // Interface para definir a estrutura do depoimento
 interface Testimonial {
   name: string;
-  message: string;
-  position: string;
-  image: string; // Novo campo para a imagem
+ image: string; // Novo campo para a imagem
 }
 
 @Component({
@@ -23,32 +22,22 @@ export class CarrosselComponent {
   testimonials: Testimonial[] = [
     {
       name: 'João Silva',
-      message: 'Esse serviço foi incrível, realmente superou minhas expectativas!',
-      position: 'CEO da Empresa A',
       image: 'assets/images/carousel1.png' // Caminho para a imagem
     },
     {
       name: 'Maria Oliveira',
-      message: 'A qualidade do atendimento é excepcional, recomendo a todos!',
-      position: 'Gerente de Marketing da Empresa B',
       image: 'assets/images/carousel2.png' // Caminho para a imagem
     },
     {
       name: 'Carlos Souza',
-      message: 'A experiência foi muito boa, sem dúvidas voltarei a utilizar.',
-      position: 'Desenvolvedor na Empresa C',
       image: 'assets/images/carousel3.png' // Caminho para a imagem
     },
     {
       name: 'Carlos Souza',
-      message: 'A experiência foi muito boa, sem dúvidas voltarei a utilizar.',
-      position: 'Desenvolvedor na Empresa C',
       image: 'assets/images/carousel4.png' // Caminho para a imagem
     },
     {
       name: 'Carlos Souza',
-      message: 'A experiência foi muito boa, sem dúvidas voltarei a utilizar.',
-      position: 'Desenvolvedor na Empresa C',
       image: 'assets/images/carousel5.png' // Caminho para a imagem
     }
   ];
