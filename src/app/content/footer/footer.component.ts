b7989805e495204f85b30b8788a2f7a40de01863
Laís Cristine Bordallo Pinheiro <laiscbordallo@gmail.com>
2024-08-17 11:30:10 -0300

    correção do botão back to top

--- src/app/content/footer/footer.component.ts
+++ src/app/content/footer/footer.component.ts
@@ -7,25 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
+
 export class FooterComponent {
-
-  constructor() { 
-
-
-document.addEventListener('DOMContentLoaded', function () {}
-  const backToTopButton = document.getElementById('back-to-top');
-
-  // Mostrar ou ocultar o botão com base na rolagem
-  window.addEventListener('scroll', function () {
-    if (window.scrollY > 300) { // Ajuste o valor conforme necessário
-      if (backToTopButton !== null) {
-        backToTopButton.style.display = 'block';
-      }
-    } else {
-      if (backToTopButton !== null) {
-        backToTopButton.addEventListener('click', function () {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-      });
-    };
-  }
+  constructor() {}
 }
